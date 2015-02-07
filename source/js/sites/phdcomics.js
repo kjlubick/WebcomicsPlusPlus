1ef56db4c60fa3567c0507bd61a23f99c4202051
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-07 09:32:16 -0500

    Fixes phdcomics

--- source/js/sites/phdcomics.js
+++ source/js/sites/phdcomics.js
@@ -2,16 +2,20 @@
 
 Mousetrap.bind('z', function() { 
 	var normal = $('img[src="images/prev_button.gif"]').closest('a')[0];
-	if (normal)
+	if (normal) {
 		normal.click();
+		return;
+	}
 	// in incognito mode
 	$('a:contains("previous")')[0].click();
 
 });
 Mousetrap.bind('c', function() { 
 	var normal = $('img[src="images/next_button.gif"]').closest('a')[0];
-	if (normal)
+	if (normal) {
 		normal.click();
+		return;
+	}
 	// in incognito mode
 	$('a:contains("next")')[0].click();
 });
@@ -21,6 +25,11 @@ Mousetrap.bind('b', function() {
 Mousetrap.bind('x', function() { 		//random
 	var rand = Math.floor((1777 * Math.random()) + 1);		//as of 1/31/15, there are 1777 comics
 	//replace numbers with my number
-	window.location.href = window.location.href.replace( /\d+/g, rand);
+	if (window.location.href.indexOf("comicid") != - 1) {
+		window.location.href = window.location.href.replace( /\d+/g, rand);
+	} else {
+		window.location.href = "http://phdcomics.com/comics/archive.php?comicid="+rand;
+	}
+	
 	
 });
